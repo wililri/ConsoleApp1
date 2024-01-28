374df2a897c380ca8fa3917bcf717343993d7108
Дилюк <@DESKTOP-5SJPVS1>
2024-01-28 10:44:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x");
+
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите y");
+
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double g = x - Math.Pow(10, Math.Sin(x)) + Math.Cos(x - y);
+
+            Console.WriteLine("g = {0:f2}", g);
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.Write("Введите длину окружности: ");
+
+            double circumference = double.Parse(Console.ReadLine());
+
+            double radius = circumference / (2 * Math.PI);
+
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Площадь круга: {0:f3}",+ area);
+        }
+    }
+}
